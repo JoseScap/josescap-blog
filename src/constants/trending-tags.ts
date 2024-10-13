5e8a109ccf980e6d64d7dc4a37fb0fa342e5ce2b
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-10-13 13:07:50 -0300

    Delete tags that does not exist yet

--- src/constants/trending-tags.ts
+++ src/constants/trending-tags.ts
@@ -1,17 +1,3 @@
-export const AZURE_TAG = "Azure";
-export const DOCKER_TAG = "Docker";
-export const DOTNET_TAG = ".Net";
-export const JAVASCRIPT_TAG = "JavaScript";
-export const REACT_TAG = "React";
-export const TYPESCRIPT_TAG = "TypeScript";
-
-export const TRENDING_TAGS: string[] = [
-  JAVASCRIPT_TAG,
-  AZURE_TAG,
-  DOCKER_TAG,
-  DOTNET_TAG,
-  REACT_TAG,
-  TYPESCRIPT_TAG,
-].sort();
+export const TRENDING_TAGS: string[] = [].sort();
 
 export default TRENDING_TAGS;
